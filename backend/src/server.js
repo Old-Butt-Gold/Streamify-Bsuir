9578eb0c14e2ae154f09455e54950677cc945a80
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-11-19 19:07:56 +0300

    feat: deployment stage

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import chatRoutes from "./routes/chat.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -22,6 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 app.listen(PORT, async () => {
     console.log(`Server running at http://localhost:${PORT}`);
